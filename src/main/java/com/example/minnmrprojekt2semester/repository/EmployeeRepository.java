539ce3b4ada0dc6a2ac086eca69e902e47f40f74
samavia ahmad <sama0641@stud.kea.dk>
2021-11-14 15:21:11 +0100

    CRUD tilføjelser

--- src/main/java/com/example/minnmrprojekt2semester/repository/EmployeeRepository.java
+++ src/main/java/com/example/minnmrprojekt2semester/repository/EmployeeRepository.java
@@ -14,33 +14,39 @@ public class EmployeeRepository {
     @Autowired //opretter instans af jdbc hver gang det benyttes
     JdbcTemplate jdbcTemplate;
 
-    public List<Employee> readAllEmployees () {
-        String sqlStatement="SELECT * FROM employee";
+    public List<Employee> readAllEmployees() {
+        String sqlStatement = "SELECT * FROM employee";
         RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<>(Employee.class);
-        return jdbcTemplate.query(sqlStatement,rowMapper);
+        return jdbcTemplate.query(sqlStatement, rowMapper);
     }
 
 
     //Create employee
-    public void createEmployee (Employee employee) {
-        String sqlStatement="INSERT into employee(jobtitle, employee_name)" +
+    public void createEmployee(Employee employee) {
+        String sqlStatement = "INSERT into employee(jobtitle, employee_name)" +
                 "VALUES(?,?)";
-        jdbcTemplate.update(sqlStatement, employee.getEmployee_name(),employee.getJobtitle());
+        jdbcTemplate.update(sqlStatement, employee.getEmployee_name(), employee.getJobtitle());
 
     }
 
 
     //read only one employee
-    public Employee readOneEmployee (int id) {
-        String sqlStatement="SELECT * FROM employee WHERE id = ?";
+    public Employee readOneEmployee(int id) {
+        String sqlStatement = "SELECT * FROM employee WHERE id = ?";
         RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<>(Employee.class);
-        return jdbcTemplate.queryForObject(sqlStatement,rowMapper,id);
+        return jdbcTemplate.queryForObject(sqlStatement, rowMapper, id);
     }
 
     //update employee
-    public void updateEmployee (Employee employee) {
-        String sqlStatement= "UPDATE employee SET " +
+    public void updateEmployee(Employee employee) {
+        String sqlStatement = "UPDATE employee SET " +
                 "jobtitle=?, employee_name =?";
-        jdbcTemplate.update(sqlStatement,employee.getJobtitle(),employee.getEmployee_name());
+        jdbcTemplate.update(sqlStatement, employee.getJobtitle(), employee.getEmployee_name());
     }
+    //slette en ansat
+    public void deleteEmployee(int id) { //id'et sepcificerer hvilken medarbejder
+        String sqlStatement = "DELETE FROM employee WHERE id =?"; //Delete VED PÅ FORHÅND at det hele rækken der skal slettes
+        jdbcTemplate.update(sqlStatement, id);
+    }
+
 }
