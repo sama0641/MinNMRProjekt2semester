977282b6842188cff03300e928263f25e7b17ef6
samavia ahmad <sama0641@stud.kea.dk>
2021-11-16 14:19:52 +0100

    CRUD http færdig

--- src/main/java/com/example/minnmrprojekt2semester/controllers/MotorhomeController.java
+++ src/main/java/com/example/minnmrprojekt2semester/controllers/MotorhomeController.java
@@ -64,7 +64,7 @@ public class MotorhomeController {
         model.addAttribute("booking_list", bookings);
         return "Booking/all_bookings";
     }
-//--------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------CUSTOMER------------------------------------------------------------------
     @GetMapping("/find_customer_by_id")
     public String findCustomerById () {
         return "Customer/find_customer_by_id";
@@ -127,14 +127,13 @@ public class MotorhomeController {
         return "redirect:get_all_customers";
     }
 
-
     @GetMapping("/delete_customer/{id}")
     public String deleteCustomer (@PathVariable("id") int id) {
         customerRepository.deleteCustomer(id);
         return  "redirect:../get_all_customers"; //går en sti tilbage således at delete bliver udskiftet med getall
     }
 
-
+//----------------------------------------------MOTORHOME---------------------------------------------
     @GetMapping("/find_motorhome_by_id")
     public String findMotorhomeById () {
         return "find_motorhome_by_id";
@@ -153,31 +152,6 @@ public class MotorhomeController {
         return "Motorhome/motorhome_by_id";
     }
 
-    @GetMapping("/update_motorhome")
-    public String updateMotorhome (Model model) {
-        model.addAttribute("motorhome_id", temporaryId);
-        return "Motorhome/update_motorhome";
-    }
-
-    @PostMapping("/direct_update_motorhome")
-    public String directUpdateMotorhome (@RequestParam(name = "is_available")boolean is_available,
-                                         @RequestParam(name = "price_per_day")int price_per_day,
-                                         @RequestParam(name = "season")String season,
-                                         @RequestParam(name = "size_category")String size_category,
-                                         @RequestParam(name = "brand_name")String brand_name,
-                                         @RequestParam(name = "model_name")String model_name,
-                                        RedirectAttributes attributes) {
-
-        attributes.addAttribute("is_available", is_available);
-        attributes.addAttribute("price_per_day", price_per_day);
-        attributes.addAttribute("season", season);
-        attributes.addAttribute("size_category", size_category);
-        attributes.addAttribute("brand_name", brand_name);
-        attributes.addAttribute("model_name", model_name);
-        motorhomeRepository.updateMotorhome(new Motorhome(is_available, price_per_day, season, size_category, brand_name, model_name ),
-                temporaryId);
-        return "redirect:/motorhome_updated/" + temporaryId;
-    }
 
     @GetMapping("/motorhome_updated/{id}")
     public String updatedMotorhome (@PathVariable("id") int id,
@@ -195,9 +169,54 @@ public class MotorhomeController {
         model.addAttribute("size_category", size_category);
         model.addAttribute("brand_name", brand_name);
         model.addAttribute("model_name", model_name);
-        return  "Customer/customer_is_updated";
+        return  "Motorhome/motorhome_is_updated";
     }
 
+    @GetMapping("/update_motorhome/{motorhome_id}")
+    public String updateMotorhome (Model model, @PathVariable("motorhome_id") int motorhomeID) {
+        model.addAttribute("motorhome", motorhomeRepository.readOneMotorhome(motorhomeID));
+        return "Motorhome/motorhome_customer";
+    }
+
+    @PostMapping("/direct_update_motorhome")
+    public String directUpdateMotorhome (@RequestParam(name = "is_available")boolean is_available,
+                                         @RequestParam(name = "price_per_day")int price_per_day,
+                                         @RequestParam(name = "season")String season,
+                                         @RequestParam(name = "size_category")String size_category,
+                                         @RequestParam(name = "brand_name")String brand_name,
+                                         @RequestParam(name = "model_name")String model_name,
+                                         RedirectAttributes attributes) {
+
+        attributes.addAttribute("customer_name", is_available);
+        attributes.addAttribute("customer_mail", price_per_day);
+        attributes.addAttribute("customer_phone_number", season);
+        attributes.addAttribute("customer_name", size_category);
+        attributes.addAttribute("customer_mail", brand_name);
+        attributes.addAttribute("customer_phone_number", model_name);
+        motorhomeRepository.updateMotorhome(new Motorhome(is_available,price_per_day,season,size_category,
+                brand_name,model_name), temporaryId);
+        return "redirect:/customer_updated/" + temporaryId;
+    }
+
+
+    @GetMapping("/create_motorhome")
+    public String createMotorhome () {
+        return "Motorhome/create_motorhome";
+    }
+
+    @PostMapping("/create_motorhome")
+    public String createMotorhome (@ModelAttribute Motorhome motorhome) {
+        motorhomeRepository.createMotorhome(motorhome);
+        return "redirect:get_all_motorhome";
+    }
+
+    @GetMapping("/delete_motorhome/{id}")
+    public String deleteMotorhome (@PathVariable("id") int id) {
+        motorhomeRepository.deleteMotorhome(id);
+        return  "redirect:../get_all_motorhomes"; //går en sti tilbage således at delete bliver udskiftet med getall
+    }
+
+    //--------------------------------------------EMPLOYEE-------------------------------------------------
     @GetMapping("/find_employee_by_id")
     public String findEmployeeById () {
         return "Employee/find_employee_by_id";
@@ -244,6 +263,25 @@ public class MotorhomeController {
         model.addAttribute("employee_name",employee_name);
         return  "Employee/employee_is_updated";
     }
+
+      @GetMapping("/create_employee")
+    public String createEmployee () {
+        return "Employee/create_employee";
+    }
+
+    @PostMapping("/create_employee")
+    public String createEmployee (@ModelAttribute Employee employee) {
+        employeeRepository.createEmployee(employee);
+        return "redirect:get_all_motorhome";
+    }
+
+    @GetMapping("/delete_employee/{id}")
+    public String deleteEmployee (@PathVariable("id") int id) {
+        employeeRepository.deleteEmployee(id);
+        return  "redirect:../get_all_employees"; //går en sti tilbage således at delete bliver udskiftet med getall
+    }
+
+
 
 
 }
